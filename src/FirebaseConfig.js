0372ada557cf7591a830745ee23c74b9af1c1d34
moenuma <moenumasawa@gmail.com>
2024-11-27 17:51:53 -0800

    Add vocab form

---
+++ src/FirebaseConfig.js
@@ -0,0 +1,18 @@
+// Firebase Configuration
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAYKwDq5PZwwyfVfG9CSJ9l32MKnxFCvwg",
+  authDomain: "jp-learning-app.firebaseapp.com",
+  projectId: "jp-learning-app",
+  storageBucket: "jp-learning-app.appspot.com",
+  messagingSenderId: "1078421967934",
+  appId: "1:1078421967934:ios:0d22fd3987f3b5e03fad6f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
